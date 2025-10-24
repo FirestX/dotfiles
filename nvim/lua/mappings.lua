4689c2407784305e6ff8bc5847e6ed1fc45220ec
Rohozin <rohozin.max@gmail.com>
2025-10-24 19:25:12 +0200

    feat: change some keybindings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,7 +8,7 @@ vim.g.mapleader = " "
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
--- FZF 
+-- FZF
 vim.keymap.set("n", "<leader>ff", require("fzf-lua").files, { noremap = true, silent = true })
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
 
@@ -21,3 +21,10 @@ map('n', '<leader>1', function() require('harpoon'):list():select(1) end, { desc
 map('n', '<leader>2', function() require('harpoon'):list():select(2) end, { desc = 'Harpoon: Jump to File 2' })
 map('n', '<leader>3', function() require('harpoon'):list():select(3) end, { desc = 'Harpoon: Jump to File 3' })
 map('n', '<leader>4', function() require('harpoon'):list():select(4) end, { desc = 'Harpoon: Jump to File 4' })
+
+-- Copilot
+vim.g.copilot_no_tab_map = true
+map('i', '<S-Tab>', function() require("copilot.suggestion").accept() end, { expr = true, replace_keycodes = false })
+
+-- Diagnostics
+map('n', '<leader>da', vim.diagnostic.open_float, { desc = 'Open Diagnostics Float' })
