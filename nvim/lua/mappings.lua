2eb1f0b2919ab6aa77147c6d0e590a55a894eeea
Rohozin <rohozin.max@gmail.com>
2025-10-29 19:16:27 +0100

    feat: add code actions

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -38,3 +38,28 @@ vim.g.copilot_no_tab_map = true
 map("i", "<M-Tab>", function()
   require("copilot.suggestion").accept_line()
 end, { expr = true, replace_keycodes = false, desc = "Accept Copilot suggestion" })
+
+-- Code Actions
+map("n", "<leader>ca", function() require("tiny-code-action").code_action() end, { noremap = true, silent = true, desc = "Code Action" })
+require("tiny-code-action").setup({
+  picker = {
+    "buffer",
+    opts = {
+      hotkeys = true, -- Enable hotkeys for quick selection of actions
+      hotkeys_mode = "text_diff_based", -- Modes for generating hotkeys
+      auto_preview = false, -- Enable or disable automatic preview
+      auto_accept = false, -- Automatically accept the selected action (with hotkeys)
+      position = "cursor", -- Position of the picker window
+      winborder = "single", -- Border style for picker and preview windows
+      keymaps = {
+        preview = "K", -- Key to show preview
+        close = { "q", "<Esc>" }, -- Keys to close the window (can be string or table)
+        select = "<CR>", -- Keys to select action (can be string or table)
+      },
+      custom_keys = {
+        { key = 'm', pattern = 'Fill match arms' },
+        { key = 'r', pattern = 'Rename.*' }, -- Lua pattern matching
+      },
+    },
+  },
+})
