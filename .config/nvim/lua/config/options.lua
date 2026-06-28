2723b293a5089e6277af1862a78a851defdb7958
Rohozin Maksym <rohozin.max@gmail.com>
2026-06-28 21:06:45 +0200

    swapped bufferline with vim's tabs

--- .config/nvim/lua/config/options.lua
+++ .config/nvim/lua/config/options.lua
@@ -1,3 +1,27 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.o.showtabline = 2
+
+-- Only show filename in tabs
+function _G.custom_tabline()
+  local s = ""
+  for i = 1, vim.fn.tabpagenr "$" do
+    local winnr = vim.fn.tabpagewinnr(i)
+    local buflist = vim.fn.tabpagebuflist(i)
+    local bufnr = buflist[winnr]
+    local bufname = vim.fn.bufname(bufnr)
+    local label = bufname ~= "" and vim.fn.fnamemodify(bufname, ":t") or "[No Name]"
+
+    if i == vim.fn.tabpagenr() then
+      s = s .. "%#TabLineSel#"
+    else
+      s = s .. "%#TabLine#"
+    end
+    s = s .. " " .. i .. ":" .. label .. " "
+  end
+  s = s .. "%#TabLineFill#"
+  return s
+end
+
+vim.o.tabline = "%!v:lua.custom_tabline()"
